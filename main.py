e37e5d32b9b032abf5fcfc27443103fe1e24792c
Pham Thuan <thuan23082004@gmail.com>
2025-10-29 15:25:22 +0700

    feat(model): add hybrid filtering implementation

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import pandas as pd
+import numpy as np
+
 from src.content_base import ContentBasedRecommender
+from src.collaborative import CF, get_dataframe_ratings_base
+from src.hybrid import HybridRecommender
 
 csv_path = "data/processed/movies_after_preprocessing.csv"
 
-recommender = ContentBasedRecommender(csv_path)
+print("Khởi tạo mô hình Content-Based Recommender...")
+content_model = ContentBasedRecommender(csv_path)
 
-movie_name = "Toy Story (1995)"
-recommendations = recommender.recommend(movie_name, 10)
+print("\nKhởi tạo mô hình Collaborative Filtering...")
+Y_data = get_dataframe_ratings_base()              
+cf_model = CF(data_matrix=Y_data, k=5, uuCF=1)     
+cf_model.normalize_matrix()
+cf_model.similarity()
 
-print(f"\nPhim tương tự với '{movie_name}':")
-for i, rec in enumerate(recommendations, start=1):
-    print(f"{i}. {rec}")
+print("\nKết hợp mô hình Hybrid...")
+hybrid = HybridRecommender(content_model, cf_model, alpha=0.6)
+
+user_id = 3
+liked_title = "Toy Story (1995)"
+top_n = 10
+
+print(f"\nGỢI Ý HYBRID CHO USER {user_id} DỰA TRÊN PHIM '{liked_title}':\n")
+recommendations = hybrid.recommend(user_id=user_id, liked_title=liked_title, top_n=top_n)
+
+
+
